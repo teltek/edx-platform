83945930aa4a187a6878c29890a5b3c677222b73
mrey <mrey@teltek.es>
2017-06-06 17:47:56 +0200

    TTK-14254 Rescale iframe on client with jquery

--- common/lib/xmodule/xmodule/js/src/pumukit/fullscreen.js
+++ common/lib/xmodule/xmodule/js/src/pumukit/fullscreen.js
@@ -1,25 +1,65 @@
-$(document).ready(function(e) {
+var total = 0;
+
+function scaleElementSize(element, size) {
+    var proportion = 1.00;
+    var scaleProportion = 'scale(1.00)';
+    var proportionWidth = $(element).parent().parent().parent().width() / $(element).width();
+    if (size != 1 && proportionWidth < 1.00) {
+        proportion = proportionWidth - 0.05;
+        scaleProportion = 'scale('+proportion+')';
+    }
+    $(element).css('-ms-zoom', proportion);
+    $(element).css('-moz-transform', scaleProportion);
+    $(element).css('-moz-transform-origin', '0 0');
+    $(element).css('-o-transform', scaleProportion);
+    $(element).css('-o-transform-origin', '0 0');
+    $(element).css('-webkit-transform', scaleProportion);
+    $(element).css('-webkit-transform-origin', '0 0');
+}
+
+function scaleIframes() {
+    el = $('body', $('#mh_iframe'));
+    if (!el.context[0]) {
+        if (total < 5000) {
+            total += 500;
+            setTimeout(scaleIframes, 500);
+            return;
+        }
+    } else {
+        total = 0;
+        var iframes = $([id=mh_iframe])[0];
+        $(iframes).each(function(d){
+            var iframed = $(iframes[d]);
+            scaleElementSize(iframed, 0);
+        });
+    }
+}
+
+$(function(){
+    total = 0;
+
+    $(document).on('click', '.button.action-primary.action-save', function(e) {
+        total = 0;
+        setTimeout(scaleIframes, 4000);
+    });
+
     $(window).on('resize', function(e){
         if (screen.width === window.innerWidth) {
-            // this is full screen
-            $('#mh_iframe').css('-ms-zoom', '1.00');
-            $('#mh_iframe').css('-moz-transform', 'scale(1.00)');
-            $('#mh_iframe').css('-moz-transform-origin', '0 0');
-            $('#mh_iframe').css('-o-transform', 'scale(1.00)');
-            $('#mh_iframe').css('-o-transform-origin', '0 0');
-            $('#mh_iframe').css('-webkit-transform', 'scale(1.00)');
-            $('#mh_iframe').css('-webkit-transform-origin', '0 0');
+            // Force fullscreen size scaling
+            var iframes = $([id=mh_iframe])[0];
+            $(iframes).each(function(d){
+                var iframed = $(iframes[d]);
+                scaleElementSize(iframed, 1);
+            });
         } else {
-            var proportion = ($('#mh_iframe').parent().parent().parent().width() / $('#mh_iframe').width()) - 0.05;
-            var scaleProportion = 'scale('+proportion+')';
-            $('#mh_iframe').css('-ms-zoom', proportion);
-            $('#mh_iframe').css('-moz-transform', scaleProportion);
-            $('#mh_iframe').css('-moz-transform-origin', '0 0');
-            $('#mh_iframe').css('-o-transform', scaleProportion);
-            $('#mh_iframe').css('-o-transform-origin', '0 0');
-            $('#mh_iframe').css('-webkit-transform', scaleProportion);
-            $('#mh_iframe').css('-webkit-transform-origin', '0 0');
+            // Rescale iframes to fit in page
+            var iframes = $([id=mh_iframe])[0];
+            $(iframes).each(function(d){
+                var iframed = $(iframes[d]);
+                scaleElementSize(iframed, 0);
+            });
         }
     });
+
+    scaleIframes();
 });
-                               
