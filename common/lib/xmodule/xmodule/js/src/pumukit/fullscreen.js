67b5568d6a589d290458f3cf407826206d9862ba
mrey <mrey@teltek.es>
2017-06-07 12:06:07 +0200

    TTK-14254: Fix rescale iframes on load, new and resize for all kind of iframes

--- common/lib/xmodule/xmodule/js/src/pumukit/fullscreen.js
+++ common/lib/xmodule/xmodule/js/src/pumukit/fullscreen.js
@@ -1,4 +1,5 @@
 var total = 0;
+var componentsTotal = 0;
 
 function scaleElementSize(element, size) {
     var proportion = 1.00;
@@ -17,52 +18,82 @@ function scaleElementSize(element, size) {
     $(element).css('-webkit-transform-origin', '0 0');
 }
 
-function scaleIframes() {
-    el = $('body', $('#mh_iframe'));
-    if (!el.context[0]) {
+function scaleElementsIframes(element, size) {
+    if (element) {
+        try {
+            $(element).each(function(d){
+                var iframed = $(element[d]);
+                scaleElementSize(iframed, size);
+            });
+        } catch (e) {
+        }
+    }
+}
+
+function scaleAllIframes(size)
+{
+    var opencastIframes = $('iframe[id="mh_iframe"]');
+    var uvigoIframes = $('iframe[src*="tv.uvigo.es"]');
+    var cmarIframes = $('iframe[src*="tv.campusdomar.es"]');
+
+    scaleElementsIframes(opencastIframes, size);
+    scaleElementsIframes(uvigoIframes, size);
+    scaleElementsIframes(cmarIframes, size);
+}
+
+function scaleIframesWhenLoaded() {
+    opencastElement = $('body', $('#mh_iframe'));
+    monostreamElement = $('body', $('iframe'));
+    if (!opencastElement.context[0] && !monostreamElement.context[0]) {
         if (total < 10000) {
             total += 500;
-            setTimeout(scaleIframes, 500);
+            setTimeout(scaleIframesWhenLoaded, 500);
             return;
         }
     } else {
         total = 0;
-        var iframes = $([id=mh_iframe])[0];
-        $(iframes).each(function(d){
-            var iframed = $(iframes[d]);
-            scaleElementSize(iframed, 0);
+        scaleAllIframes(0);
+    }
+}
+
+function defineListener() {
+    var pumukitButton = $('.single-template.add-xblock-component-button[data-type=pumukit]');
+    if (pumukitButton.length != 1) {
+        if (componentsTotal < 10000) {
+            componentsTotal += 500;
+            setTimeout(defineListener, 500);
+            return;
+        }
+    } else {
+        $('.single-template.add-xblock-component-button[data-type=pumukit]').click(function(e) {
+            total = 0;
+            setTimeout(scaleIframesWhenLoaded, 4000);
         });
     }
 }
 
 $(function(){
     total = 0;
+    componentsTotal = 0;
 
     $(document).on('click', '.button.action-primary.action-save', function(e) {
         total = 0;
-        setTimeout(scaleIframes, 4000);
+        setTimeout(scaleIframesWhenLoaded, 4000);
     });
 
     $(window).on('resize', function(e){
         if (screen.width === window.innerWidth) {
             // Force fullscreen size scaling
-            var iframes = $([id=mh_iframe])[0];
-            $(iframes).each(function(d){
-                var iframed = $(iframes[d]);
-                scaleElementSize(iframed, 1);
-            });
+            scaleAllIframes(1);
         } else {
             // Rescale iframes to fit in page
             try {
-                var iframes = $([id=mh_iframe])[0];
-                $(iframes).each(function(d){
-                    var iframed = $(iframes[d]);
-                    scaleElementSize(iframed, 0);
-                });
+                scaleAllIframes(0);
             } catch (e) {
             }
         }
     });
 
-    scaleIframes();
+    scaleIframesWhenLoaded();
+    defineListener();
 });
