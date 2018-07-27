09cb1fd1990b1cbe7454ac7d4cac1eb4b8ca1a26
mrey <mrey@teltek.es>
2018-07-27 17:49:24 +0200

    TTK-18537: wip PRINT CERTIFICATE TO PDF

--- lms/djangoapps/certificates/pdf.py
+++ lms/djangoapps/certificates/pdf.py
@@ -18,6 +18,8 @@ from reportlab.lib.units import mm
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph
 from reportlab.platypus.tables import Table, TableStyle
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from xmodule.assetstore.assetmgr import AssetManager
@@ -66,6 +68,7 @@ class PDFCertificate(object):
         """
         self.pdf = Canvas(file_buffer, pagesize=letter)
         y_pos = self.draw_logos()
+        y_pos = self.add_text(y_pos)
 
         self.pdf.showPage()
         self.pdf.save()
@@ -157,4 +160,49 @@ class PDFCertificate(object):
                 )
 
         return img_y_pos - self.min_clearance
+
+
+    def add_text(self, y_pos):
+        first_line = _('NATIONAL UNIVERSITY OF DISTANCE EDUCATION')
+        paragraph = (_('The Rector of the National University of Distance Education, considering that {student_name} with National Identity Number: {student_national_id} has successfully finished the UNED Abierta course {course_title} According to the program on the back of this document, issues the present CERTIFICATE OF USE')).format(student_name="Student Name", student_national_id="123456789X", course_title="Title of the course")
+        date_cert = _('26th July 2018')
+        rector_title = _('The Rector of the UNED,')
+        rector_name = 'Alejandro Tiana Ferrer'
+        credits_number_line = (_('Credits number: {course_credits} ETCS')).format(course_credits=1.0)
+        course_effort_line = (_('Hours number: {course_effort} hours')).format(course_effort=25)
+        law_line = _('This degree is given as suitable of UNED and it does not have the official nature established in number 30 of the Organic Law 4/2007 that modifies the article 34 of Organic Law 6/2001 of Universities')
+        auth_line = (_('* The authenticity of this document, as well as its validity and validity, can be checked through the following URL: {cert_url}')).format(cert_url='https://example.com')
+
+        id_label = 'testing-label-id'
+        item_id = 'testing-item-id'
+        title = first_line
+
+        log.error('Available fonts: {0}'.format(self.pdf.getAvailableFonts()))
+
+        # pdfmetrics.registerFont(TTFont('Fontana-Semibold', '../../static/certificates/fonts/Fontana/Fontana-ND-Cc-OsF-Semibold.otf'))
+        # self.pdf.setFont('Fontana-Semibold', 18)
+
+        vertical_padding = 5 * mm
+        horizontal_padding_from_border = self.margin + 18 * mm
+        font_size = 18
+        self.pdf.setFontSize(font_size)
+        self.pdf.drawString(horizontal_padding_from_border, y_pos - vertical_padding - font_size / 2, title)
+        y_pos = y_pos - vertical_padding - font_size / 2 - self.min_clearance
+
+        horizontal_padding_from_border = self.margin + 11 * mm
+        font_size = 12
+        self.pdf.setFontSize(font_size)
+        y_pos = y_pos - font_size / 2 - vertical_padding
+        # Draw Order/Invoice No.
+        self.pdf.drawString(horizontal_padding_from_border, y_pos,
+                            _(u'{id_label} # {item_id}').format(id_label=id_label, item_id=item_id))
+        y_pos = y_pos - font_size / 2 - vertical_padding
+        # Draw Date
+        # self.pdf.drawString(
+        #     horizontal_padding_from_border, y_pos, _(u'Date: {date}').format(date=self.date)
+        # )
+
+        y_pos = y_pos - self.min_clearance
+
+        return y_pos
 
