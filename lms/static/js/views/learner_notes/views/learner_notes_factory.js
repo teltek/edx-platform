78eef7a221fdf19909f3275b597d00920acb3e83
mrey <mrey@teltek.es>
2018-08-21 17:58:10 +0200

    TTK-18545: Add learner notes page first version

---
+++ lms/static/js/views/learner_notes/views/learner_notes_factory.js
@@ -0,0 +1,113 @@
+(function(define, undefined) {
+    'use strict';
+    define([
+        'gettext',
+        'jquery',
+        'underscore',
+        'backbone',
+        'logger',
+        'edx-ui-toolkit/js/pagination/paging-collection',
+        'js/student_account/models/user_account_model',
+        'js/student_account/models/user_preferences_model',
+        'js/views/fields',
+        'js/learner_notes/views/learner_notes_fields',
+        'js/learner_notes/views/learner_notes_view',
+        'js/student_profile/models/badges_model',
+        'js/student_profile/views/badge_list_container',
+        'js/student_account/views/account_settings_fields',
+        'js/views/message_banner',
+        'string_utils'
+    ], function(gettext, $, _, Backbone, Logger, PagingCollection, AccountSettingsModel, AccountPreferencesModel,
+                 FieldsView, LearnerNotesFieldsView, LearnerNotesView, BadgeModel, BadgeListContainer,
+                 AccountSettingsFieldViews, MessageBannerView) {
+        return function(options) {
+            var learnerNotesElement = $('.wrapper-profile');
+
+            var accountSettingsModel = new AccountSettingsModel(
+                _.extend(
+                    options.account_settings_data,
+                    {'default_public_account_fields': options.default_public_account_fields}
+                ),
+                {parse: true}
+            );
+            var AccountPreferencesModelWithDefaults = AccountPreferencesModel.extend({
+                defaults: {
+                    account_privacy: options.default_visibility
+                }
+            });
+            var accountPreferencesModel = new AccountPreferencesModelWithDefaults(options.preferences_data);
+
+            accountSettingsModel.url = options.accounts_api_url;
+            accountPreferencesModel.url = options.preferences_api_url;
+
+            var editable = options.own_profile ? 'toggle' : 'never';
+
+            var messageView = new MessageBannerView({
+                el: $('.message-banner')
+            });
+
+            var BadgeCollection = PagingCollection.extend({
+                queryParams: {
+                    currentPage: 'current_page'
+                }
+            });
+            var badgeCollection = new BadgeCollection();
+            badgeCollection.url = options.badges_api_url;
+
+            var badgeListContainer = new BadgeListContainer({
+                'attributes': {'class': 'badge-set-display'},
+                'collection': badgeCollection,
+                'find_courses_url': options.find_courses_url,
+                'ownProfile': options.own_profile,
+                'badgeMeta': {
+                    'badges_logo': options.badges_logo,
+                    'backpack_ui_img': options.backpack_ui_img,
+                    'badges_icon': options.badges_icon
+                }
+            });
+
+            var learnerNotesView = new LearnerNotesView({
+                el: learnerNotesElement,
+                ownProfile: options.own_profile,
+                has_preferences_access: options.has_preferences_access,
+                accountSettingsModel: accountSettingsModel,
+                preferencesModel: accountPreferencesModel,
+                badgeListContainer: badgeListContainer
+            });
+
+            var getProfileVisibility = function() {
+                if (options.has_preferences_access) {
+                    return accountPreferencesModel.get('account_privacy');
+                } else {
+                    return accountSettingsModel.get('profile_is_public') ? 'all_users' : 'private';
+                }
+            };
+
+            var showLearnerNotesView = function() {
+                // Record that the profile page was viewed
+                Logger.log('edx.user.settings.viewed', {
+                    page: 'profile',
+                    visibility: getProfileVisibility(),
+                    user_id: options.profile_user_id
+                });
+
+                // Render the view for the first time
+                learnerNotesView.render();
+            };
+
+            if (options.has_preferences_access) {
+                if (accountSettingsModel.get('requires_parental_consent')) {
+                    accountPreferencesModel.set('account_privacy', 'private');
+                }
+            }
+            showLearnerNotesView();
+
+            return {
+                accountSettingsModel: accountSettingsModel,
+                accountPreferencesModel: accountPreferencesModel,
+                learnerNotesView: learnerNotesView,
+                badgeListContainer: badgeListContainer
+            };
+        };
+    });
+}).call(this, define || RequireJS.define);
---
+++ lms/static/js/views/learner_notes/views/learner_notes_views.js
@@ -0,0 +1,103 @@
+(function(define, undefined) {
+    'use strict';
+    define([
+        'gettext', 'jquery', 'underscore', 'backbone', 'edx-ui-toolkit/js/utils/html-utils',
+        'common/js/components/views/tabbed_view',
+        'js/student_profile/views/section_two_tab',
+        'text!templates/learner_notes/learner_notes.underscore'],
+        function(gettext, $, _, Backbone, HtmlUtils, TabbedView, SectionTwoTab, learnerNotesTemplate) {
+            var LearnerNotesView = Backbone.View.extend({
+
+                initialize: function(options) {
+                    this.options = _.extend({}, options);
+                    _.bindAll(this, 'showFullProfile', 'render', 'renderFields', 'showLoadingError');
+                    this.listenTo(this.options.preferencesModel, 'change:' + 'account_privacy', this.render);
+                    var Router = Backbone.Router.extend({
+                        routes: {':accomplishments': 'loadTab'}
+                    });
+
+                    this.router = new Router();
+                    this.firstRender = true;
+                },
+
+                template: _.template(learnerNotesTemplate),
+
+                showFullProfile: function() {
+                    if (this.options.ownProfile) {
+                        return this.options.preferencesModel.get('account_privacy') === 'all_users';
+                    } else {
+                        return this.options.accountSettingsModel.get('profile_is_public');
+                    }
+                },
+
+                setActiveTab: function(tab) {
+                // This tab may not actually exist.
+                    if (this.tabbedView.getTabMeta(tab).tab) {
+                        this.tabbedView.setActiveTab(tab);
+                    }
+                },
+
+                render: function() {
+                    var tabs,
+                        self = this;
+
+                    HtmlUtils.setHtml(this.$el, HtmlUtils.template(learnerNotesTemplate)({
+                        username: self.options.accountSettingsModel.get('username'),
+                        ownProfile: self.options.ownProfile,
+                        showFullProfile: self.showFullProfile()
+                    }));
+                    this.renderFields();
+
+                    if (this.showFullProfile() && (this.options.accountSettingsModel.get('accomplishments_shared'))) {
+                        tabs = [
+                            {
+                                view: this.options.badgeListContainer,
+                                title: gettext('Accomplishments'),
+                                url: 'accomplishments'
+                            }
+                        ];
+
+                        // Build the accomplishments Tab and fill with data
+                        this.options.badgeListContainer.collection.fetch().done(function() {
+                            self.options.badgeListContainer.render();
+                        }).error(function() {
+                            self.options.badgeListContainer.renderError();
+                        });
+
+                        this.tabbedView = new TabbedView({
+                            tabs: tabs,
+                            router: this.router,
+                            viewLabel: gettext('Profile')
+                        });
+
+                        this.tabbedView.render();
+                        this.$el.find('.account-settings-container').append(this.tabbedView.el);
+
+                        if (this.firstRender) {
+                            this.router.on('route:loadTab', _.bind(this.setActiveTab, this));
+                            Backbone.history.start();
+                            this.firstRender = false;
+                            // Load from history.
+                            this.router.navigate((Backbone.history.getFragment() || 'about_me'), {trigger: true});
+                        } else {
+                            // Restart the router so the tab will be brought up anew.
+                            Backbone.history.stop();
+                            Backbone.history.start();
+                        }
+                    }
+                    return this;
+                },
+
+                renderFields: function() {
+                    var view = this;
+                },
+
+                showLoadingError: function() {
+                    this.$('.ui-loading-indicator').addClass('is-hidden');
+                    this.$('.ui-loading-error').removeClass('is-hidden');
+                }
+            });
+
+            return LearnerNotesView;
+        });
+}).call(this, define || RequireJS.define);
