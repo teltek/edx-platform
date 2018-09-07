b894ebbf1f255d9b0e7eb150d1fbdb871c5c64f5
mrey <mrey@teltek.es>
2018-09-07 12:03:47 +0200

    TTK-18920: Fix bug on loading certificates list

--- lms/djangoapps/certificates/apis/v0/serializers.py
+++ lms/djangoapps/certificates/apis/v0/serializers.py
@@ -6,6 +6,10 @@ from rest_framework import serializers
 from certificates.models import GeneratedCertificate
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
+import logging
+
+log = logging.getLogger(__name__)
+
 class CertificateClassSerializer(serializers.ModelSerializer):
     """
     Serializer for CertificateClass model.
@@ -18,9 +22,17 @@ class CertificateClassSerializer(serializers.ModelSerializer):
         fields = ('course_id', 'verify_uuid', 'mode', 'grade', 'created_date', 'image_url', 'display_name')
 
     def get_image_url(self, certificate):
-        course_overview = CourseOverview.get_from_id(certificate.course_id)
-        return course_overview.image_urls['small']
+        try:
+            course_overview = CourseOverview.get_from_id(certificate.course_id)
+            return course_overview.image_urls['small']
+        except Exception as excep:
+            log.warning('There was an exception on get course overview: {0}'.format(excep.message))
+        return 'ImageNotFound'
 
     def get_display_name(self, certificate):
-        course_overview = CourseOverview.get_from_id(certificate.course_id)
-        return course_overview.display_name
+        try:
+            course_overview = CourseOverview.get_from_id(certificate.course_id)
+            return course_overview.display_name
+        except Exception as excep:
+            log.warning('There was an exception on get course overview: {0}'.format(excep.message))
+        return 'Course Not Found'
